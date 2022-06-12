44796bb9abf20a461b4f11912a2693593772aae8
Guillermo <memocargo@hotmail.com>
2022-06-12 14:42:34 -0500

    App para servicios creada y models creados tambien, url de media fixed y carpeta creadad

---
+++ curso_display/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Curso(models.Model):
+    titulo = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    imagen = models.ImageField(upload_to='curso_display')
+    nombrePlataforma = models.CharField(max_length=50, blank=True)
+    enlace = models.URLField(max_length=200 , blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = 'curso'
+        verbose_name_plural = 'cursos'
+        
+    def __str__(self):
+        return self.titulo
