7563d303dc5132ddcdc171a80dc6f35ab3da586b
JoseD1903 <106483491+JoseD1903@users.noreply.github.com>
2022-06-13 17:59:45 -0500

    Add files via upload
    arreglando

---
+++ cursos/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('login', views.login, name='login'),
+    path('', views.inicio, name='inicio'),
+    path('profesor/', views.profesor, name='profesor'),
+    path('fpassword/', views.fpassword, name='fpassword'),
+    path('registro/', views.registro, name='registro'),
+    path('cursosdepython/', views.cpython, name='cursosdepython'),
+    path('cursosdeandroidstudio/', views.candroid, name= 'cursosdeandroidstudio')
+]
+
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
